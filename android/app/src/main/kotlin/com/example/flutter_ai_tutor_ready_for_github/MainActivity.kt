de43eb1058392e42542de2cb8c2149548c870993
vovan2266 <vovan.bryant24@yandex.ru>
2025-09-05 21:47:59 +0300

    Complete Android structure with embedding v2

--- android/app/src/main/kotlin/com/example/flutter_ai_tutor_ready_for_github/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_ai_tutor_ready_for_github/MainActivity.kt
@@ -1,10 +1,3 @@
 ﻿package com.example.flutter_ai_tutor_ready_for_github
-
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity: FlutterActivity()
